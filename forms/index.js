a51635919cb2f956bd7368964196818c5bc5997a
kunxin-chor <chorkunxin@yahoo.com>
2021-03-16 09:54:24 +0000

    Added how to create product

---
+++ forms/index.js
@@ -0,0 +1,49 @@
+// import in caolan forms
+const forms = require("forms");
+// create some shortcuts
+const fields = forms.fields;
+const validators = forms.validators;
+
+var bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="alert alert-error help-block">' + object.error + '</div>' : '';
+
+    var validationclass = object.value && !object.error ? 'has-success' : '';
+    validationclass = object.error ? 'has-error' : validationclass;
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group ' + validationclass + '">' + label + widget + error + '</div>';
+};
+
+const createProductForm = () => {
+    return forms.create({
+        'name': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'cost': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'description': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+    })
+};
+
+module.exports = { createProductForm, bootstrapField };
--- routes/products.js
+++ routes/products.js
@@ -2,14 +2,36 @@ const express = require("express");
 const router = express.Router();
 
 // import in the Product model
-const {Product} = require('../models')
+const { Product } = require('../models');
 
-router.get('/', async (req,res)=>{
+// import in the Forms
+const { bootstrapField, createProductForm } = require('../forms');
+
+router.get('/', async (req, res) => {
     let products = await Product.collection().fetch();
-    console.log(products);
     res.render('products/index', {
         'products': products.toJSON()
     })
 })
+
+router.get('/create', async (req, res) => {
+    const productForm = createProductForm();
+    res.render('products/create',{
+        'form': productForm.toHTML(bootstrapField)
+    })
+})
+
+router.post('/create', async(req,res)=>{
+    const productForm = createProductForm();
+    productForm.handle(req, {
+        'success': async (form) => {
+            const product = new Product(form.data);
+            await product.save();
+            res.redirect('/products');
+
+        }
+    })
+})
+
 
 module.exports = router;
