e4e91d13bf4a143aed25440599f502b62436de14
kunxin-chor <chorkunxin@yahoo.com>
2021-03-22 10:16:13 +0000

    Able to display uploaded imdage

--- routes/cloudinary.js
+++ routes/cloudinary.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const cloudinary = require("cloudinary")
 
 router.get('/sign', (req,res)=>{
-    const params_to_sign = req.query.params_to_sign;
+    const params_to_sign = JSON.parse(req.query.params_to_sign);
     const api_secret = process.env.CLOUDINARY_API_SECRET;
     console.log(params_to_sign);
     const signature = cloudinary.utils.api_sign_request(params_to_sign, api_secret);
