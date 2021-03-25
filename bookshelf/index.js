122a73223df3dc2c77b2a8b43ef6b18d96c039c8
kunxin-chor <chorkunxin@yahoo.com>
2021-03-25 08:32:31 +0000

    shifted database credientals to .env files

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,10 +1,11 @@
 // Setting up the database connection
 const knex = require('knex')({
-  client: 'mysql',
+  client: process.env.DB_DRIVER,
   connection: {
-    user: 'foo',
-    password: 'bar',
-    database: 'organic'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST
   }
 })
 const bookshelf = require('bookshelf')(knex)
