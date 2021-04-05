d1261bc4db090a7364aac5dd0be1434fcab2d79e
kunxin-chor <chorkunxin@yahoo.com>
2021-04-05 04:30:34 +0000

    Added code for heroku

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -5,7 +5,8 @@ const knex = require('knex')({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST
+    host: process.env.DB_HOST,
+    ssl: true
   }
 })
 const bookshelf = require('bookshelf')(knex)
